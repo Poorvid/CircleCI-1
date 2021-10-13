124a338fa4917bffc263d596cb39185bf503afbd
Poorvi Dharwad <poorvid@cybage.com>
2021-10-13 10:52:14 +0530

    commit-1

---
+++ src/test/java/circleci/test/firstTest.java
@@ -0,0 +1,32 @@
+package circleci.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+public class firstTest {
+
+	WebDriver driver;
+
+	@BeforeTest
+	void setupClass()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	
+	@Test
+	void test() {
+		driver.get("https://www.google.com/");
+		driver.quit();
+	}
+
+}
